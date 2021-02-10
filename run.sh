77cbf09bdd14dc4f3d2ff4210c6058e0afc92ac3
PointCloudYC <cyinac@connect.ust.hk>
2021-02-10 23:15:22 +0800

    update

--- run.sh
+++ run.sh
@@ -6,8 +6,8 @@ python train_semseg.py --model pointnet2_sem_seg --test_area 5 --log_dir pointne
 python test_semseg.py --log_dir pointnet2_sem_seg --test_area 5 --visual
 
 # modelnet40
-python train_cls.py --model pointnet2_cls_ssg --normal --log_dir pointnet2_cls_ssg --batch_size 24 --epoch 200
-python test_cls.py --normal --log_dir pointnet2_cls_ssg
+# python train_cls.py --model pointnet2_cls_ssg --normal --log_dir pointnet2_cls_ssg --batch_size 24 --epoch 200
+# python test_cls.py --normal --log_dir pointnet2_cls_ssg
 # can not run MSG model since out-of-memory error of CUDA
 # python train_cls.py --model pointnet2_cls_msg --normal --log_dir pointnet2_cls_msg
 
